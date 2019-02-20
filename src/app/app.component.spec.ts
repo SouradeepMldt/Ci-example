c3ba3f207f82fc7404bef766798001abd7b6b6a2
SouradeepMldt <souradeep.mldt@gmail.com>
2019-02-20 21:50:21 +0530

    Title change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Ci-example'`, () => {
+  it(`should have as title 'Boon Software'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Ci-example');
+    expect(app.title).toEqual('Boon Software');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Ci-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Boon Software!');
   });
 });
