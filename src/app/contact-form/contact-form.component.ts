abd2c336aafbd39f1b5491f051fd913f3c225cb5
Boondb <souradeep.mldt@gmail.com>
2019-02-28 16:54:58 +0530

    Reactive Form Add

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,8 +7,8 @@ import { FormControl } from '@angular/forms';
 })
 export class ContactFormComponent  {
 
-  name = new FormControl('');   //create a new instance of the form control to save as a class property.
-                                //Use the constructor of FormControl to set its initial value, which in this case is an empty string. By creating these controls in your component class, you get immediate access to listen for, update, and validate the state of the form input.
+  name = new FormControl(''); //create a new instance of the form control to save as a class property.
+//Use the constructor of FormControl to set its initial value, which in this case is an empty string. By creating these controls in your component class, you get immediate access to listen for, update, and validate the state of the form input.
 
 /*updateName() {
   this.name.setValue('Nancy');
